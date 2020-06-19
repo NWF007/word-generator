0c9a713663f15887e9ac6a64499f25f33425e3fd
Nico <ncfortuin27@gmail.com>
2020-06-19 15:25:16 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import arrayWords from '../utils/words';
+import countriesArray from '../utils/countries';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+  words = '';
+  limit = 10;
+  countries = '';
+
+  handleSlideChange(newLimit: number){
+    this.limit = newLimit;
+  }
+
+  generate(){
+    this.words = arrayWords.slice(0, this.limit).join(" ");
+  }
+
+  randomCountries(){
+    this.countries = countriesArray.slice(0,Math.random()*15).join(" ");
+  }
+
 }
---
+++ src/utils/countries.ts
@@ -0,0 +1,18 @@
+// some random words
+export default [
+    'South Africa',
+    'USA',
+    'India',
+    'England',
+    'France',
+    'Congo',
+    'Namibia', 
+    'Pakistan',
+    'UAE',
+    'Mexico',
+    'Brazil',
+    'Italy',
+    'China',
+    'Russia'
+  ];
+  
